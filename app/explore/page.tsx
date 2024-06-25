2dccf7528df75b39eb9be204b47f95cf62831dd2
MarkoPvlkvc <pavlakovic.marko@gmail.com>
2024-06-26 00:30:02 +0200

    explore navbar fix

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -180,7 +180,7 @@ const ExploreClient = () => {
 
   return (
     <main className="flex flex-col items-center">
-      <nav className="mt-5 grid h-fit w-[85%] max-w-4xl grid-cols-[auto_1fr] items-center rounded-full bg-black/75 backdrop-blur-lg md:mt-10 lg:mt-20 lg:w-full">
+      <nav className="z-20 mt-5 grid h-fit w-[85%] max-w-4xl grid-cols-[auto_1fr] items-center rounded-full md:mt-10 lg:mt-20 lg:w-full">
         <Link href="/">
           <img src="CroTrends-cropped.svg" className="mx-7 my-[18px] h-7" />
         </Link>
